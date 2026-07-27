a70e70983d828cba1588926da3e9fe20a47e4a4f
Amer Cosovic <cosovic14@gmail.com>
2026-07-27 03:06:28 -0400

    add Google Ads tag to landing site

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
 import "@/styles/globals.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=AW-18352666930"
+        strategy="afterInteractive"
+      />
+      <Script id="google-ads" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-18352666930');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
